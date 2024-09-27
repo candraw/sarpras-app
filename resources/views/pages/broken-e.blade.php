fd2d5384020c271b0034904384120d03c3eb1646
dimasgtcndr_ <candrawibowo350@gmail.com>
2024-09-27 14:41:50 +0700

    commit1

--- resources/views/pages/broken-e.blade.php
+++ resources/views/pages/broken-e.blade.php
@@ -6,7 +6,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Data Barang Rusak</h1>
+            <h1 class="m-0">Data Peralatan / Elektronik / Mesin Rusak</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
